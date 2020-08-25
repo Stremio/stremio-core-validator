c62394f147f7322c60d97a6b2056f7d77767aba0
nklhrstv <me@nikolahristov.com>
2020-08-25 17:48:24 +0300

    added validation functions for Stream and MetaDetail

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 use serde::{Deserialize, Serialize};
 use stremio_core::types::addons::Manifest;
-use stremio_core::types::MetaPreview;
+use stremio_core::types::{MetaDetail, MetaPreview, Stream};
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
 fn validate<T: Serialize + for<'a> Deserialize<'a>>(data: &JsValue) -> JsValue {
     JsValue::from_serde(&data.into_serde::<T>().ok()).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn manifest(data: &JsValue) -> JsValue {
+    validate::<Manifest>(data)
 }
 
 #[wasm_bindgen]
@@ -13,6 +18,11 @@ pub fn meta_preview(data: &JsValue) -> JsValue {
 }
 
 #[wasm_bindgen]
-pub fn manifest(data: &JsValue) -> JsValue {
-    validate::<Manifest>(data)
+pub fn meta_detail(data: &JsValue) -> JsValue {
+    validate::<MetaDetail>(data)
+}
+
+#[wasm_bindgen]
+pub fn stream(data: &JsValue) -> JsValue {
+    validate::<Stream>(data)
 }
