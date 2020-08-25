88976ee8acd3968ad1958e507fcd2d6310e8fad2
nklhrstv <me@nikolahristov.com>
2020-08-25 17:44:00 +0300

    manifest validation implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+use serde::{Deserialize, Serialize};
+use stremio_core::types::addons::Manifest;
 use stremio_core::types::MetaPreview;
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
+fn validate<T: Serialize + for<'a> Deserialize<'a>>(data: &JsValue) -> JsValue {
+    JsValue::from_serde(&data.into_serde::<T>().ok()).unwrap()
+}
+
 #[wasm_bindgen]
 pub fn meta_preview(data: &JsValue) -> JsValue {
-    JsValue::from_serde(&data.into_serde::<MetaPreview>().ok()).unwrap()
+    validate::<MetaPreview>(data)
+}
+
+#[wasm_bindgen]
+pub fn manifest(data: &JsValue) -> JsValue {
+    validate::<Manifest>(data)
 }
